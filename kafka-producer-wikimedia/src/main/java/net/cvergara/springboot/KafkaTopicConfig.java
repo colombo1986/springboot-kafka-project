db5c1c0d1250f9eb950f9ee23868f70db057468a
Cristopher Vergara <c.vergara@nlnexus.com>
2023-07-09 21:45:01 -0400

    Initial commit

---
+++ kafka-producer-wikimedia/src/main/java/net/cvergara/springboot/KafkaTopicConfig.java
@@ -0,0 +1,23 @@
+package net.cvergara.springboot;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.internals.Topic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name(topicName)
+                .build();
+    }
+
+
+}
